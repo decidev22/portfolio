8078ed53562ef8000255b98a0fc6b3e81840244f
decidev22 <hamento22@gmail.com>
2024-03-29 11:20:13 +1100

    Loading page version 1

---
+++ app/component/animation/AnimationText.tsx
@@ -0,0 +1,54 @@
+"use client";
+import { useState, useEffect } from "react";
+import { motion } from "framer-motion";
+
+interface AnimatedTextProps {
+  text: string;
+  className?: string;
+}
+
+// Takes two arguments, text and className prop, text: new line char with \n, className for styles
+const AnimatedText: React.FC<AnimatedTextProps> = ({
+  text,
+  className = "",
+}) => {
+  const [displayText, setDisplayText] = useState<React.ReactNode[]>(
+    []
+  );
+  const [index, setIndex] = useState(0);
+
+  useEffect(() => {
+    if (index < text.length) {
+      const delay = setTimeout(() => {
+        if (text[index] + text[index + 1] === "\\n") {
+          setDisplayText((previous) => [
+            ...previous,
+            <br key={index} />,
+          ]);
+          setIndex((currentIndex) => currentIndex + 2);
+        } else if (text[index] === " ") {
+          setDisplayText((previous) => [...previous, " "]);
+          setIndex((currentIndex) => currentIndex + 1);
+        } else {
+          setDisplayText((previous) => [
+            ...previous,
+            <motion.div
+              whileHover={{ scale: 1.2 }}
+              onHoverStart={(e) => {}}
+              onHoverEnd={(e) => {}}
+              className="inline-block"
+              key={index}
+            >
+              {text[index]}
+            </motion.div>,
+          ]);
+          setIndex((currentIndex) => currentIndex + 1);
+        }
+      }, 90);
+      return () => clearTimeout(delay);
+    }
+  }, [index, text]);
+  return <div className={`${className}`}>{displayText}</div>;
+};
+
+export default AnimatedText;
