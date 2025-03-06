7c5ee99b4897222db8d363f7ab3c95a9723943a9
decidev22 <hamento22@gmail.com>
2025-03-06 23:48:27 +1100

    rework on nav bar with news panel

--- app/component/nav/NavBar.tsx
+++ app/component/nav/NavBar.tsx
@@ -1,17 +1,21 @@
 "use client";
 import NavLink from "./NavLink";
 import classes from "./NavBar.module.css";
+
+import SomeNews from "./SomeNews";
+
 const NavBar = () => {
   return (
-    <nav className="flex justify-end w-full top-0 p-3 sticky z-50 h-[55px] bg-black border border-1 border-white">
-      <ul className="list-none">
+    <nav className="flex items-center w-full top-0 p-5 sticky z-50 h-[55px] bg-black">
+      <div className="flex">
+        <SomeNews news={["I'm open for new opportunities!", "Remote ANZ or Sydney NSW"]} />
+      </div>
+      <ul className="flex ml-auto list-none">
         <NavLink href="/" title="Home" />
         <NavLink href="/products" title="Products" />
         <NavLink href="/blogs" title="Blog" />
         <NavLink href="/contact" title="Contact" />
       </ul>
-
-      {/* New Nav Bar */}
     </nav>
   );
 };
