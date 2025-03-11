c2e7b282d9716ade166ed125efbda65fb0cd981a
decidev22 <hamento22@gmail.com>
2025-03-11 22:10:30 +1100

    Navbar redesign

--- app/component/nav/NavBar.tsx
+++ app/component/nav/NavBar.tsx
@@ -9,15 +9,23 @@ import SomeNews from "./SomeNews";
 const NavBar = () => {
   return (
     <nav className="flex items-center w-full top-0 sticky z-50 h-[55px] bg-black">
-      <div className="flex">
+      <div className="flex-1">
         <SomeNews news={["I'm open for new opportunities!", "Remote ANZ or Sydney NSW"]} reactComponent={<SydneyOperaHouse />} />
       </div>
-      <ul className="flex ml-auto list-none">
+      <ul className="hidden lg:flex ml-auto list-none ">
         <NavLink href="/" title="Home" />
         <NavLink href="/products" title="Products" />
         <NavLink href="/blogs" title="Blog" />
         <NavLink href="/contact" title="Contact" />
       </ul>
+      <button type="button" className="flex font-bold justify-end lg:hidden">
+        <div className="grid grid-cols-2 grid-rows-2 text-center text-[12px] border border-1 border-white w-[35px] h-[35px]">
+          <div className="flex items-center justify-center">M</div>
+          <div className="flex items-center justify-center">E</div>
+          <div className="flex items-center justify-center">N</div>
+          <div className="flex items-center justify-center">U</div>
+        </div>
+      </button>
     </nav>
   );
 };
