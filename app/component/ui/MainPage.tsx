d31055e1e75fcd8db4d08a2aa8642b6f5989ab5f
decidev22 <hamento22@gmail.com>
2025-03-19 01:03:49 +1100

    add events to the main page

--- app/component/ui/MainPage.tsx
+++ app/component/ui/MainPage.tsx
@@ -1,8 +1,39 @@
+import { useEffect, useState } from "react";
 import ActivityBox from "../github_components/ActivityBox";
 import Main_Greeting from "../pages/mainPageComponent/Main_Greeting";
 import classes from "./MainPage.module.css";
 
 const MainPageLayout = () => {
+  const [githubActivities, setGithubActivities] = useState<any[]>([]);
+  const [loading, setLoading] = useState<boolean>(true);
+  const [error, setError] = useState<string | null>(null);
+
+  useEffect(() => {
+    const fetchGithubActivities = async () => {
+      const lambdaUrl = process.env.NEXT_PUBLIC_GET_GITHUB_ACTIVITIES_LAMBDA_URL;
+      if (!lambdaUrl) {
+        setError("Lambda URL is missing in environment variables.");
+        setLoading(false);
+        return;
+      }
+
+      try {
+        const response = await fetch(lambdaUrl);
+        if (!response.ok) {
+          throw new Error("Failed to fetch GitHub activities");
+        }
+
+        const data = await response.json();
+        setGithubActivities(data);
+        setLoading(false);
+      } catch (error: any) {
+        setError(error.message);
+        setLoading(false);
+      }
+    };
+
+    fetchGithubActivities();
+  }, []);
   return (
     <>
       <div className="flex w-full px-5 2xl:!flex-row justify-center place-items-baseline mr-[100px] sm:flex-col">
@@ -12,11 +43,16 @@ const MainPageLayout = () => {
         <div className="2xl:mt-0 ml-[100px]  sm: mt-10 w-min">
           <div className={`absolute ${classes.activityBoxContainer}`}>
             <div>
+              {loading ? <p>Loading...</p> : null}
+              {error ? <p>Error loading :&lt;</p> : null}
               <p>I am doing...</p>
-              <ActivityBox type="PullRequestEvent" content="Portfolio" />
-              <ActivityBox type="PushEvent" content="Portfolio" />
-              <ActivityBox type="CreateEvent" content="Portfolio" />
-              <ActivityBox type="WatchEvent" content="Portfolio" />
+              {githubActivities.length > 0 ? (
+                githubActivities.map((activity: any, index: any) => (
+                  <ActivityBox key={index} type={activity.type} content={activity.repo_name} />
+                ))
+              ) : (
+                <p>No recent activities.</p>
+              )}
             </div>
           </div>
         </div>
