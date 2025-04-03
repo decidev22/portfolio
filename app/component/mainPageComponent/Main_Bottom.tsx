8985f5e90f7454823b0e1475e27e97e9e50a4337
decidev22 <hamento22@gmail.com>
2025-04-04 00:00:54 +1100

    Go backend for game

--- app/component/mainPageComponent/Main_Bottom.tsx
+++ app/component/mainPageComponent/Main_Bottom.tsx
@@ -1,7 +1,7 @@
 const MainBottom = () => {
   return (
     <div className="absolute inset-x-0 bottom-0 w-full h-min grid grid-cols-9 z-10 bg-black">
-      <div className="col-span-3 text-center">This is footer</div>
+      <div className="col-span-3 text-center">Message Me Here!</div>
       <div className="col-span-3 text-center">2025</div>
       <div className="col-span-3 text-center">Andy Baeck</div>
     </div>
---
+++ app/game/page.tsx
@@ -0,0 +1,10 @@
+export default function Game() {
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="flex flex-col">
+        <div className="top-1 text-center">Title: Node War!</div>
+        <div className="w-[500px] h-[500px] border border-1 border-white"> Game Box </div>
+      </div>
+    </div>
+  );
+}
